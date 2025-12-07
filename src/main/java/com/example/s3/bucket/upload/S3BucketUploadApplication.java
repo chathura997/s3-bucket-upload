23c3af833792ae062890b83dbde85fbf1de12999
chathura <chathuradinushka97@gmail.com>
2025-12-07 15:10:18 +0530

    first commit

---
+++ src/main/java/com/example/s3/bucket/upload/S3BucketUploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.s3.bucket.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3BucketUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3BucketUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s3/bucket/upload/S3BucketUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s3.bucket.upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3BucketUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
